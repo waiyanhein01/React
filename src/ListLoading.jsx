cc4fca30ab726e504d19feabeafc2d519179c02a
waiyanhein01 <wyan00196@gmail.com>
2024-09-02 23:55:14 +0630

    Loading fixed

--- src/ListLoading.jsx
+++ src/ListLoading.jsx
@@ -2,11 +2,45 @@ import React from "react";
 
 const ListLoading = () => {
   return (
-    <div className="bg-gray-50 rounded-lg flex justify-between items-center border border-zinc-700 p-3 mt-3 mb-3 duration-200">
-      <div className=" gap-2 flex">
-        <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
-        <span className=" bg-neutral-500 w-32 h-3 lg:w-40 lg:h-4 rounded-md"></span>
-        <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded-full"></span>
+    <div className=" mt-3 mb-3 animate-pulse flex gap-3 flex-col">
+      <div className=" gap-2 flex justify-between items-center bg-gray-300 p-3 rounded-lg">
+        <div className="flex gap-2">
+          <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
+          <span className=" bg-neutral-500 w-32 h-3 lg:w-44 lg:h-4 rounded-md"></span>
+        </div>
+        <span className=" bg-neutral-500 w-5 h-6 lg:w-5 lg:h-6 rounded"></span>
+      </div>
+
+      <div className=" gap-2 flex justify-between items-center bg-gray-300 p-3 rounded-lg">
+        <div className="flex gap-2">
+          <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
+          <span className=" bg-neutral-500 w-32 h-3 lg:w-44 lg:h-4 rounded-md"></span>
+        </div>
+        <span className=" bg-neutral-500 w-5 h-6 lg:w-5 lg:h-6 rounded"></span>
+      </div>
+
+      <div className=" gap-2 flex justify-between items-center bg-gray-300 p-3 rounded-lg">
+        <div className="flex gap-2">
+          <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
+          <span className=" bg-neutral-500 w-32 h-3 lg:w-44 lg:h-4 rounded-md"></span>
+        </div>
+        <span className=" bg-neutral-500 w-5 h-6 lg:w-5 lg:h-6 rounded"></span>
+      </div>
+
+      <div className=" gap-2 flex justify-between items-center bg-gray-300 p-3 rounded-lg">
+        <div className="flex gap-2">
+          <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
+          <span className=" bg-neutral-500 w-32 h-3 lg:w-44 lg:h-4 rounded-md"></span>
+        </div>
+        <span className=" bg-neutral-500 w-5 h-6 lg:w-5 lg:h-6 rounded"></span>
+      </div>
+
+      <div className=" gap-2 flex justify-between items-center bg-gray-300 p-3 rounded-lg">
+        <div className="flex gap-2">
+          <span className=" bg-neutral-500 w-3 h-3 lg:w-4 lg:h-4 rounded"></span>
+          <span className=" bg-neutral-500 w-32 h-3 lg:w-44 lg:h-4 rounded-md"></span>
+        </div>
+        <span className=" bg-neutral-500 w-5 h-6 lg:w-5 lg:h-6 rounded"></span>
       </div>
     </div>
   );
