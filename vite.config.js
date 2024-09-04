82331e2b1170efb7e8ecf7f1f8368ab2d4ecb0de
waiyanhein01 <wyan00196@gmail.com>
2024-09-04 20:00:32 +0630

    version: 1.0.6

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+      '/api': 'http://localhost:5000'
+    }
+  }
 });
